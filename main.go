e37c743cc4c18dc88188d9966858381dd69e94de
movsb <chkesp@gmail.com>
2021-10-01 05:00:12 +0800

    multiple paths supported

--- main.go
+++ main.go
@@ -17,26 +17,92 @@ import (
 )
 
 type Data struct {
-	Content string `json:"content"`
-	Version int64  `json:"version"`
+	Content      string `json:"content"`
+	Version      int64  `json:"version"`
+	lastAccessed time.Time
 }
 
+type Cache struct {
+	data   map[string]*Data
+	mu     sync.RWMutex
+	ticker *time.Ticker
+}
+
+// TODO: We don't need the full request, path is enough.
+func (c *Cache) Get(r *http.Request) *Data {
+	path := r.URL.Path
+	c.mu.Lock()
+	defer c.mu.Unlock()
+	d, ok := c.data[path]
+	if !ok {
+		d = &Data{}
+		c.data[path] = d
+	}
+	d.lastAccessed = time.Now()
+	return d
+}
+
+func (c *Cache) Set(r *http.Request, data *Data) {
+	path := r.URL.Path
+	c.mu.Lock()
+	defer c.mu.Unlock()
+	data.lastAccessed = time.Now()
+	c.data[path] = data
+}
+
+func (c *Cache) tidy() {
+	c.mu.Lock()
+	defer c.mu.Unlock()
+	toDelete := make([]string, 0)
+	for k, d := range c.data {
+		if time.Since(d.lastAccessed) > cacheTimeout {
+			toDelete = append(toDelete, k)
+		}
+	}
+	for _, k := range toDelete {
+		delete(c.data, k)
+	}
+}
+
+func NewCache() *Cache {
+	c := &Cache{
+		data:   make(map[string]*Data),
+		ticker: time.NewTicker(cacheCheckInterval),
+	}
+	// TODO close ticker.
+	go func() {
+		for range c.ticker.C {
+			c.tidy()
+		}
+	}()
+	return c
+}
+
+const (
+	cacheTimeout       = time.Hour * 24
+	cacheCheckInterval = time.Hour
+	maxSize            = 1 << 20
+	defaultPort        = 7962
+)
+
 var (
-	d  Data
-	mu sync.RWMutex
+	//go:embed index.html
+	indexDotHTML   []byte
+	cache          = NewCache()
+	reMaybeBrowser = regexp.MustCompile(`Mozilla|Firefox|Chrome|Safari`)
 )
 
 func handleSync(w http.ResponseWriter, r *http.Request) {
 	if !isBrowser(r) {
-		b, err := ioutil.ReadAll(io.LimitReader(r.Body, 1<<20))
+		b, err := ioutil.ReadAll(io.LimitReader(r.Body, maxSize))
 		if err != nil {
 			http.Error(w, err.Error(), http.StatusBadRequest)
 			return
 		}
-		mu.Lock()
-		d.Version = time.Now().Unix()
+		d := cache.Get(r)
 		d.Content = string(b)
-		mu.Unlock()
+		d.Version = time.Now().Unix()
+		cache.Set(r, d)
 		return
 	}
 
@@ -46,10 +112,7 @@ func handleSync(w http.ResponseWriter, r *http.Request) {
 		return
 	}
 
-	// json.Decode may last some time on network, don't block.
-	mu.Lock()
-	defer mu.Unlock()
-
+	d := cache.Get(r)
 	if dd.Version < d.Version {
 		// client is too old, send what we have.
 		w.WriteHeader(http.StatusConflict)
@@ -57,31 +120,22 @@ func handleSync(w http.ResponseWriter, r *http.Request) {
 	} else if dd.Version == d.Version {
 		// versions match. nothing to do.
 		if d.Version == 0 {
-			d = dd
+			cache.Set(r, &dd)
 		}
 		w.WriteHeader(http.StatusNoContent)
 	} else {
 		// update content.
 		w.WriteHeader(http.StatusNoContent)
-		d = dd
+		cache.Set(r, &dd)
 	}
 }
-
-//go:embed index.html
-var indexDotHTML []byte
-
-var reMaybeBrowser = regexp.MustCompile(`Mozilla|Firefox|Chrome|Safari`)
-
 func isBrowser(r *http.Request) bool {
 	return reMaybeBrowser.MatchString(r.Header.Get(`User-Agent`))
 }
 
 func indexHTML(w http.ResponseWriter, r *http.Request) {
 	if !isBrowser(r) {
-		mu.Lock()
-		s := d.Content
-		mu.Unlock()
-
+		s := cache.Get(r).Content
 		w.Header().Set(`Content-Type`, `text/plain`)
 		io.Copy(w, strings.NewReader(s))
 		return
@@ -105,7 +159,7 @@ func index(w http.ResponseWriter, r *http.Request) {
 }
 
 func main() {
-	p := flag.Int(`p`, 7962, `port`)
+	p := flag.Int(`p`, defaultPort, `port`)
 	flag.Parse()
 	http.HandleFunc(`/`, index)
 	addr := fmt.Sprintf(`0.0.0.0:%d`, *p)
